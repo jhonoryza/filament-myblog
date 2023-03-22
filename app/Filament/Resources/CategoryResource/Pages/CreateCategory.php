894b8cd3ce02e2dff63a84057112a24b30b45d73
fajar sidik priatna <jardik.oryza@gmail.com>
2023-03-22 18:16:52 +0700

    add categories crud

---
+++ app/Filament/Resources/CategoryResource/Pages/CreateCategory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Resources\CategoryResource\Pages;
+
+use App\Filament\Resources\CategoryResource;
+use Filament\Forms\Components\TextInput;
+use Filament\Pages\Actions;
+use Filament\Resources\Form;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCategory extends CreateRecord
+{
+    protected static string $resource = CategoryResource::class;
+
+    protected function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required(),
+            ]);
+    }    
+}
